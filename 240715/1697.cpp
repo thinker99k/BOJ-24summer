4609ecbbfcfd5b03dca6524e9c146d8788dd6673
Kunhee, Lee <kitsune03@outlook.com>
2024-07-16 02:21:36 +0900

    dd

---
+++ 240715/1697.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, k;
+
+bool arr[100001];
+bool visited[100001];
+
+bool val(int pos){
+    if(pos < 0 || pos >= 100001){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+int bfs(){
+    visited[]
+
+    return 1;
+}
+
+int main(){
+    cin >> n >> k;
+
+    cout << bfs();
+}
---
+++ 240715/2178(2darrbfs).cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int lrow, lcol;
+char arr[101][101];
+bool visited[101][101];
+bool checked[101][101];
+int steps[101][101];
+
+typedef struct {
+    int row;
+    int col;
+} Pos;
+
+bool val(int irow, int icol) {
+    // 1. 범위 맞나 검사
+    if (irow < 0 || irow >= lrow || icol < 0 || icol >= lcol) {
+        return false;
+    }
+
+    // 2. 길 맞나 검사
+    if (arr[irow][icol] == '0') {
+        return false;
+    }
+
+    // 3. 방문 했나 검사
+    if (visited[irow][icol]) {
+        return false;
+    }
+
+    // 4. 체크 했나 검사
+    if (checked[irow][icol]) {
+        return false;
+    }
+
+    return true;
+}
+
+void bfs() {
+    queue<Pos> q;
+
+    Pos startp = {0, 0};
+    q.push(startp);
+
+    int phase = 1;
+    int loops = 1;
+    while (true) {
+        int nextloops = 0;
+
+        for (int i = 0; i < loops; i++) {
+            Pos pp = q.front();
+            q.pop();
+            visited[pp.row][pp.col] = true;
+            checked[pp.row][pp.col] = true;
+
+            steps[pp.row][pp.col] = phase;
+
+            // 상
+            if (val(pp.row - 1, pp.col)) {
+                Pos tmp = {pp.row - 1, pp.col};
+                q.push(tmp);
+                checked[pp.row-1][pp.col] = true;
+                nextloops++;
+            }
+
+            // 하
+            if (val(pp.row + 1, pp.col)) {
+                Pos tmp = {pp.row + 1, pp.col};
+                q.push(tmp);
+                checked[pp.row+1][pp.col] = true;
+                nextloops++;
+            }
+
+            // 좌
+            if (val(pp.row, pp.col - 1)) {
+                Pos tmp = {pp.row, pp.col - 1};
+                q.push(tmp);
+                checked[pp.row][pp.col-1] = true;
+                nextloops++;
+            }
+
+            // 우
+            if (val(pp.row, pp.col + 1)) {
+                Pos tmp = {pp.row, pp.col + 1};
+                q.push(tmp);
+                checked[pp.row][pp.col+1] = true;
+                nextloops++;
+            }
+        }
+
+        ++phase;
+        loops = nextloops;
+
+        if (q.empty()) break;
+    }
+}
+
+
+int main() {
+    cin >> lrow >> lcol;
+
+    for (int i = 0; i < lrow; i++) {
+        cin >> arr[i];
+    }
+
+    bfs();
+
+    cout << steps[lrow - 1][lcol - 1];
+}
